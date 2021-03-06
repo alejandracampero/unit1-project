e8aa9b7e057b0997a1da217afa42eb50b64ec512
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-03-06 17:34:22 -0500

    add convertCardToNumber and compareFlipped functions

--- js/main.js
+++ js/main.js
@@ -85,8 +85,56 @@ function handleFlip() {
         p2flipEl.classList.replace('outline', player2Flip);
         p2flipEl.classList.add('animated', 'zoomInRight');
     }
-    
+    compareFlipped()
+    render()
+
 };
+
+function compareFlipped() {
+    if (covertCardToNumber(player1Flip) > covertCardToNumber(player2Flip)) {
+        player1Stack.push(`${player1Flip}`);
+        player1Stack.push(`${player2Flip}`);
+        player2Stack.splice(player2Stack.length, 1);
+    } else if (covertCardToNumber(player1Flip) < covertCardToNumber(player2Flip)) {
+        player2Stack.push(`${player2Flip}`);
+        player2Stack.push(`${player1Flip}`);
+        player1Stack.splice(player1Stack.length, 1);
+    }
+    console.log(`Player 1 has ${player1Stack.length} cards` )
+    console.log(`Player 2 has ${player2Stack.length} cards`)
+};
+
+//CALLBACK function meant to translate the card description to a number in order to compare
+function covertCardToNumber(card) {
+    if(`${card}` === 'd02' || `${card}` === 'c02' || `${card}` === 's02' || card === 'h02'){
+      return 2;
+    }else if (`${card}`=== 'd03' || `${card}` === 'c03' || `${card}` === 's03' || `${card}` === 'h03') {
+        return 3;
+    }else if (`${card}`=== 'd04' || `${card}`=== 'c04' || `${card}` === 's04' || `${card}` === 'h04') {
+        return 4;
+    }else if (`${card}` === 'd05' || `${card}` === 'c05' || `${card}` === 's05' || `${card}` === 'h05') {
+        return 5;
+    }else if (`${card}`=== 'd06' || `${card}` === 'c06' || `${card}` === 's06' || `${card}` === 'h06') {
+        return 6;
+    }else if (`${card}` === 'd07' || `${card}` === 'c07' || `${card}` === 's07' || `${card}` === 'h07') {
+        return 7;
+    }else if (`${card}`=== 'd08' || `${card}` === 'c08' || `${card}` === 's08' || `${card}` === 'h08') {
+        return 8;
+    }else if (`${card}` === 'd09' || `${card}` === 'c09' || `${card}` === 's09' || `${card}` === 'h09') {
+        return 9;
+    }else if (`${card}`=== 'd10' || `${card}` === 'c10' || `${card}` === 's10' || `${card}` === 'h10') {
+        return 10;
+    }else if (`${card}` === 'dJ' || `${card}` === 'cJ' || `${card}` === 'sJ' || `${card}` === 'hJ') {
+        return 11;
+    }else if (`${card}`=== 'dQ' || `${card}` === 'cQ' || `${card}` === 'sQ' || `${card}` === 'hQ') {
+        return 12;
+    }else if (`${card}` === 'dK' || `${card}` === 'cK' || `${card}` === 'sK' || `${card}` === 'hK') {
+        return 13;
+    }else{
+        return 14;
+    }
+}
+
 
 /*------------------------- Render Functions -------------------------*/
 
