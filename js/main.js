284728c7b6cd5d8e8bf49b1c15c917f9d0dc3d56
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-03-06 11:17:39 -0500

    add shuffle and deal cards function

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ const cards = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d0
 
 
 /*------------------------- Vars -------------------------*/
-let player1stack = [];
-let player2stack = [];
+let player1Stack = [];
+let player2Stack = [];
 let player1Flip = [];
 let player2Flip = [];
 
@@ -14,18 +14,57 @@ let player2Flip = [];
 
 /*------------------------- Cached elements  -------------------------*/
 
-const p1deck = document.getElementById('p1deck');
-const p2deck = document.getElementById('p2deck');
-const p1flip = document.getElementById('p1flip');
-const p2flip = document.getElementById('p2flip');
-const dealButton = document.getElementById('deal-btn');
-const flipButton = document.getElementById('flip-btn');
+// const p1deckEl = document.getElementById('p1deck');
+// const p2deckEl= document.getElementById('p2deck');
+// const p1flipEl = document.getElementById('p1flip');
+// const p2flipEl = document.getElementById('p2flip');
+// const dealButton = document.getElementById('deal-btn');
+// const flipButton = document.getElementById('flip-btn');
 
 
 
 /*------------------------- Event Listeners  -------------------------*/
-dealButton.addEventListener('click', handleDeal);
-flipButton.addEventListener('click', handleclick );
+// dealButton.addEventListener('click', handleDeal);
+// flipButton.addEventListener('click', handleclick );
 
 /*------------------------- Functions  -------------------------*/
+
+
+  function shuffleCards() {
+    let temp = null
+    for (let = idx = cards.length - 1; idx > 0; idx -= 1) {
+        let rndIdx = Math.floor(Math.random() * (idx + 1))
+        temp = cards[idx]
+        cards[idx] = cards[rndIdx]
+        cards[rndIdx] = temp
+    }
+    console.log (cards)
+    return cards
+  }
+
+
+  function dealCards(){
+    shuffleCards();
+    if (cards.length > 0) {
+        for (let i = 0; i < 26; i++) {
+            let cardDealt1;
+            cardDealt1 = cards.splice(0, 1);
+            player1Stack.push(cardDealt1);
+            player1Stack = player1Stack.flat()
+        }
+        for (let i = 0; i < 26; i++) {
+            let cardDealt2;
+            cardDealt2 = cards.splice(0, 1);
+            player2Stack.push(cardDealt2);
+            player2Stack = player2Stack.flat()
+        }
+    }
+    console.log(player1Stack)
+    console.log(player2Stack)
+  }
+
+dealCards()
+
+
+
 
